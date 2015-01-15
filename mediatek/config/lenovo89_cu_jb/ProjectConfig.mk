72d26c41539b85989098265059d6711e50df72cb
Ivan Rusanov <ivan.b.rusanov@mail.com>
2015-01-15 03:35:01 +0200

    Add bma2xx accelerometer driver and set as default (same as S150 stock kernel)

--- mediatek/config/lenovo89_cu_jb/ProjectConfig.mk
+++ mediatek/config/lenovo89_cu_jb/ProjectConfig.mk
@@ -84,7 +84,7 @@ CUSTOM_HAL_SUB_IMGSENSOR =
 CUSTOM_HAL_SUB_LENS = dummy_lens
 
 # accelerometer sensor to detect accelerometer from x y z axis.
-CUSTOM_KERNEL_ACCELEROMETER = bma222E
+CUSTOM_KERNEL_ACCELEROMETER = bma2xx #bma222E
 
 # ALSPS sensor driverto detect ambint light and the object is close or far awary from device
 CUSTOM_KERNEL_ALSPS = APDS9930
